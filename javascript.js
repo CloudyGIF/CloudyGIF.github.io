2f1621cf2d5877ff9964ab5d564f6518394ef781
CloudyGIF <anna22post@gmail.com>
2025-05-30 02:00:30 +0100

    added js

---
+++ javascript.js
@@ -0,0 +1,35 @@
+const HorizontalText = document.querySelectorAll(".text-scroll");
+
+const languages = [
+    "C#",
+    "JavaScript",
+    "SQL",
+    "HTML",
+    "CSS",
+    "Python"
+  ];
+
+  function initializeItems() {
+    HorizontalText.forEach((scrollItem) => {
+      scrollItem.innerHTML = "";
+    });
+};
+
+  initializeItems();
+
+
+  languages.forEach((item) => {
+    addItemToScrolls(item);
+  });
+
+function addItemToScrolls(input) {
+ 
+    HorizontalText.forEach((scrollItem) => {
+      const textItem = document.createElement("span");
+      textItem.classList.add("text");
+      textItem.innerHTML = input;
+      scrollItem.appendChild(textItem);
+      console.log(input);
+    });
+    
+    };
